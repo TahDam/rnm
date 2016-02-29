6452ffa565a308cd8b0c173b61e0e105aecc1d1e
TahDam <alexandr.v.zaharchuk@gmail.com>
2016-02-29 22:30:23 +0300

    Draft of the function for converting of the numerical part of the file name to string and back.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
+#include <string.h>
+
+
+
+// char *strpbrk(const char *s, const char *accept);	            находит первое вхождение любого символа, перечисленного в accept
 
 using namespace std;
 
-int main() {
-    cout << "Hello, World!" << endl;
+int main(int argc, char **argv) {
+    string tmp = strpbrk(argv[1],"_");
+    tmp.erase(0,1);
+
+    int num = atoi(tmp.c_str());
+    num++;
+    string tmp2 = to_string(num);
+
+    cout<<tmp<<endl;
+    cout<<num<<endl;
+    cout<<tmp2<<endl;
+
     return 0;
+
 }
